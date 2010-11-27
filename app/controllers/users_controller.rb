f1006eb72dc6679b28be6d3f71d7f9c8ba1896ff
Majid Al Otaibi <majid.alotaibi@gmail.com>
2010-11-27 04:53:59 +0300

    Added user factory and tests for show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   
+ 
+ def show
+   @user = User.find(params[:id])     
+ end
+ 
+ 
   def new
     @title = "Sign up"
   end
-  
-  def show
-    @user = User.find(params[:id])     
-  end
+ 
   
 
 end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+  user.name                  "Majid Al Otaibi"
+  user.email                 "majid@icompusys.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+  
+end
